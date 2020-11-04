e6508c8267209ca3a3014bb135104e4f8eed6d40
PWParker01 <54680917+PWParker01@users.noreply.github.com>
2020-11-03 21:13:50 -0600

    Adding project assets
    assets include scripts, scenes, and materials used in project. Can import into project easily with drag and drop in Unity 2d project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+	public float jumpHeight = 4;
+	public float timeToJumpApex = .4f;
+	float accelerationTimeAirborne = .2f;
+	float accelerationTimeGrounded = .1f;
+	float moveSpeed = 6;
+
+	float gravity;
+	float jumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	Controller2D controller;
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Trigger!");
+		print ("in trigger");
+		if(other.gameObject.CompareTag("DeathField")){
+			transform.position = new Vector3(-8 , 0, 0);
+			
+		}
+	}
+
+	void Start() {
+		controller = GetComponent<Controller2D> ();
+
+		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		
+	}
+
+	void Update() {
+
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
+			velocity.y = jumpVelocity;
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
